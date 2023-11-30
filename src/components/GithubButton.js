734641dbe9b5df318ea12f5f52ff88d03afdf69e
jackoubridge <jaoubridge@gmail.com>
2023-11-30 16:35:37 +0000

    Added meta tags for SEO

--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -1,9 +1,14 @@
 const GitHubButton = (props) => {
+
+    function handleClick(){
+      window.open('https://github.com/jackoubridge/' + props.repo, '_blank').focus();
+    }
+
     return (
       <>
       <button 
         type='button' 
-        onClick={(e) => {e.preventDefault();window.open('https://github.com/jackoubridge/' + props.repo, '_blank').focus();}}
+        onClick={handleClick}
         className='github-button m-0 me-2 my-auto p-1 text-dark btn btn-light btn-lg align-middle d-flex justify-content-between align-items-center' title={`https://github.com/jackoubridge/${props.repo}`}>
           <span className='h-100 p-0'>Visit Repository</span>
           <span className="icon p-0 material-symbols-outlined">captive_portal</span>
