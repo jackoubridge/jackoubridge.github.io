94cd8230271429c234b9959c5565f15f9ff86cd0
jackoubridge <jaoubridge@gmail.com>
2023-12-06 15:37:32 +0000

    Tweaked footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
       <>
       <footer className="myfooter position-relative bottom-0 m-0 p-0 pt-1 pb-1 justify-content-center d-flex">
         <span className='text-end footer-text pe-2 border-end border-dark user-select-none'>© 2023 Jack Oubridge</span>
-        <span className="text-start footer-text ps-2 user-select-none">Icons from <a className="text-dark" rel="noreferrer" href="https://www.flaticon.com" target="_blank">www.flaticon.com</a></span>
+        <span className="text-start footer-text ps-2 user-select-none">Icons from <a className="text-dark" rel="noreferrer" href="https://flaticon.com" target="_blank">Flaticon</a></span>
       </footer>
       </>
     )
