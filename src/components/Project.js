392f1907c1c90f8b7340622a9b865eeadb856693
jackoubridge <jaoubridge@gmail.com>
2023-12-06 01:35:15 +0000

    Updated styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,13 +16,14 @@ const Project = (props) => {
         <div className='card projectcard w-100 h-100 text-light' style={{backgroundColor: props.color}}>
           <div className='card-header justify-content-between w-100 user-select-none align-middle ps-3 pe-3 pt-1 pb-1'>
             {props.title}
-            <img src={props.imglink} loading="lazy" className='header-img m-0 p-0' title="https://flaticon.com" alt=""></img>
+            <img src={props.imglink} loading="lazy" className='header-img m-0 p-0' alt=""></img>
           </div>
-          <div className='card-body w-100'>
+          <div className='card-body pb-0 w-100'>
             <span className='project-info d-flex user-select-none w-100 p-0'>
               {props.text}
             </span>
-            <div className='project-bottom align-middle d-flex justify-content-between position-absolute bottom-0 start-0 w-100'>
+            <hr className="mb-2 mt-3 mx-auto p-0"></hr>
+            <div className='project-bottom pb-2 align-middle d-flex justify-content-between w-100'>
               <div className='badges w-auto h-100 d-flex align-middle rounded'>
                 {list}
               </div>
