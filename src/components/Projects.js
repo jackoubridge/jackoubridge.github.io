d3cf4552a094d10b5a9ea66ae521563749ae724f
jackoubridge <jaoubridge@gmail.com>
2023-12-12 07:34:01 +0000

    Style tweaks. Added ghost card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ const Projects = React.forwardRef((props, ref) => {
       <>
       <div className='projects p-3 container-fluid text-white' ref={ref}>
 
-        <div className='row w-auto d-flex m-0 p-0 justify-content-center'>
+        <div className='row w-auto d-flex m-0 p-0 projects-row'>
           <Header title="My Projects"/>
           <Project 
             title='3D Graphics Engine' 
@@ -20,7 +20,7 @@ const Projects = React.forwardRef((props, ref) => {
           <Project 
             title='Microservices' 
             repo='Microservices' 
-            color='DarkSlateGray' 
+            color='#015c5c' 
             langs='Go' 
             imglink='https://cdn-icons-png.flaticon.com/512/9673/9673387.png' 
             text="Shazam-like system using microservices. Employs a RESTful API to identify songs from audio fragments, comparing them with a database and determining the played song from a snippet."
@@ -28,7 +28,7 @@ const Projects = React.forwardRef((props, ref) => {
           <Project
             title='Full-Stack Web App' 
             repo='Flask-App' 
-            color='DarkSlateBlue' 
+            color='MidnightBlue' 
             langs='JavaScript, Python'
             imglink='https://cdn-icons-png.flaticon.com/512/1336/1336494.png' 
             text="Using SQLAlchemy and Socket.IO, this full-stack app supports a database of users (via Steam login) and real-time simulation gambling. This was my first endeavour into the world of ReactJS and Bootstrap."
@@ -36,7 +36,7 @@ const Projects = React.forwardRef((props, ref) => {
           <Project 
             title='Internet of Things' 
             repo="IoT" 
-            color="Indigo" 
+            color="#2e2e2e" 
             langs="C++" 
             imglink="https://cdn-icons-png.flaticon.com/512/4673/4673990.png" 
             text="DIY web server that allows clients on the local network to communicate wirelessly with the Wi-Fi module and change the colour of an LED. Firmware written in Arduino IDE, hardware secured with an elastic band and a hair tie."
@@ -52,7 +52,7 @@ const Projects = React.forwardRef((props, ref) => {
           <Project
             title='Game Development' 
             repo='unity-project' 
-            color='Peru' 
+            color='#d16a0a' 
             langs='C++' 
             imglink='https://cdn-icons-png.flaticon.com/512/686/686589.png' 
             text='A first-person shooter Unity project that includes a CharacterController, object spawning and physics, and Ray Casting.'
@@ -60,7 +60,7 @@ const Projects = React.forwardRef((props, ref) => {
           <Project
             title="Evolutionary Algorithm" 
             repo='Sudoku-EA' 
-            color='MediumPurple' 
+            color='#6f52ab' 
             langs='Python' 
             imglink='https://cdn-icons-png.flaticon.com/512/11918/11918995.png' 
             text="This implementation of a famous problem-solving method uses genetic mutation and crossover to find the best Sudoku board given a random starting population."
@@ -72,6 +72,15 @@ const Projects = React.forwardRef((props, ref) => {
             langs='JavaScript' 
             imglink='https://cdn-icons-png.flaticon.com/512/12630/12630918.png' 
             text='This very site... how meta. Uses ReactJS and Bootstrap to achieve a useful and visually stunning site. Featured in this app is a card that reads "This very site... how meta. Uses ReactJS and Bootstrap to achieve a useful and visually stunning site. Featured in this app is a card tha-'
+          />
+          <Project
+            title='' 
+            repo='' 
+            color='Transparent' 
+            langs='' 
+            imglink='' 
+            text=''
+            addClass="hidden"
           />
         </div>
       </div>
