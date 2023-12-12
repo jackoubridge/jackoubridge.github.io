3de3cdebffbeb43aad836a0bb6043098aa108033
jackoubridge <jaoubridge@gmail.com>
2023-12-12 03:39:45 +0000

    Fixed responsiveness issues

--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -10,7 +10,7 @@ const GitHubButton = (props) => {
         type='button' 
         onClick={handleClick}
         className='github-button m-0 h-100 p-1 text-dark btn btn-light btn-lg align-middle d-flex justify-content-between align-items-center' title={`https://github.com/jackoubridge/${props.repo}`}>
-          <span className='h-100 p-0'>Visit Repository</span>
+          <span className='h-100 p-0 visit-text'>Visit Repository</span>
           <span className="icon p-0 material-symbols-outlined">captive_portal</span>
       </button>
       </>
