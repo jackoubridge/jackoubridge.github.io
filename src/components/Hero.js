bc29b286a7276ed6892d56d22581329ae8c7b334
jackoubridge <jaoubridge@gmail.com>
2023-12-15 01:59:23 +0000

    Changed zoom-out behaviour

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -59,70 +59,19 @@ const Hero = React.forwardRef(({ scrollToProjects }, ref) => {
 
           {/* HERO RIGHT */}
           <div className='hero-r row m-0 p-3 col-md-6 col-sm-12'>
-            <div className='col-md-4 col-sm-4 m-0 p-0'>
+            <div className='col-md-4 col-sm-4 m-0 p-0 imgcol d-flex justify-content-center'>
               <img className='hero-img my-auto w-100 h-100 rounded' loading='lazy' src={mugshot} alt=""></img>
             </div>
             <div className='col-md-8 col-sm-8 m-0 p-0 ps-2 d-flex justify-content-center mylist'>
-              {/* <div className='h-100 w-100 m-0 p-0 bg-dark rounded test my-auto'></div> */}
               <ul class="list-group ps-0 pe-0 my-auto">
                 <li class="list-group-item exeter">University of Exeter Graduate</li>
                 <li class="list-group-item">- Persistent Learner</li>
                 <li class="list-group-item">- Passionate Programmer</li>
                 <li class="list-group-item">- Solution Architect</li>
-                {/* <li class="list-group-item">Vestibulum at eros</li> */}
               </ul>
             </div>
           </div>
         </div>
-  
-        {/* <div className='row h-100 w-100 m-0 p-0 z-1 text-black'>
-  
-          <div className='hero-left d-flex justify-content-center align-middle col-md-6 col-sm-12 m-0 p-0 vh-75'>
-            <div className='hero-left-content my-auto m-0 p-2 rounded user-select-none'>
-              <span className='d-block p-2 display-2 w-100 hi-im-jack'>
-                Hi, I'm Jack
-              </span>
-              <span className='d-block m-0 p-2 h5'>
-                Software developer. Passionate programmer.
-              </span>
-              <div className='hero-buttons row m-0 p-0 w-100 d-flex'>
-                <button 
-                  type="button" 
-                  onClick={() => handleClick('https://www.linkedin.com/in/jackoubridge')}
-                  className="hero-button external-button m-2 btn btn-lg border border-black text-black align-middle d-flex justify-content-between align-items-center">
-                    <span className='h-100 p-0 pe-2'>Linkedin </span>
-                    <span className="icon p-0 material-symbols-outlined">captive_portal</span>
-                </button>
-                <button
-                  type="button" 
-                  onClick={() => handleClick('https://www.github.com/jackoubridge')}
-                  className="hero-button external-button m-2 btn btn-lg border border-black text-black align-middle d-flex justify-content-between align-items-center">
-                    <span className='h-100 p-0 pe-2'>Github</span>
-                    <span className="icon p-0 material-symbols-outlined">captive_portal</span>
-                </button>
-              </div>
-            </div>
-          </div>
-  
-          <div className='hero-right d-flex align-middle col-md-6 col-sm-12 m-0 p-0 vh-75'>
-            <div className='hero-right-content m-0 p-2 rounded user-select-none'>
-              <div className='row w-100 h-100 m-0 p-0'>
-                <div className='col-4 m-0 p-0 h-100 d-flex'>
-                  <img className='hero-img my-auto w-100 h-100 rounded' loading='lazy' src={mugshot} alt=""></img>
-                </div>
-                <div className='col-8 m-0 p-0 my-auto bullet'>
-                  <ul>
-                    <li className='p-1 ps-2 pe-2 d-inline exeter border border-light'>University of Exeter Graduate</li>
-                    <li className='p-1 mt-2'>— Persistent Learner</li>
-                    <li className='p-1'>— Passionate Programmer</li>
-                    <li className='p-1'>— Solution Architect</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-  
-        </div> */}
 
         <span onClick={scrollToProjects} className="user-select-none position-absolute scroll-button material-symbols-outlined">expand_circle_down</span>
       </div>
