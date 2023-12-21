346a819f7509fc1289eee4be7f6c7bc5398526d2
jackoubridge <jaoubridge@gmail.com>
2023-12-21 00:42:46 +0000

    Updated card content & styling

--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -1,7 +1,14 @@
 const GitHubButton = (props) => {
 
+    var link = props.url;
+
+    if(props.type === 'Repository'){
+      link = 'https://github.com/jackoubridge/' + props.url;
+    }
+
+
     function handleClick(){
-      window.open('https://github.com/jackoubridge/' + props.repo, '_blank').focus();
+      window.open(link, '_blank').focus();      
     }
 
     return (
@@ -9,8 +16,8 @@ const GitHubButton = (props) => {
       <button 
         type='button' 
         onClick={handleClick}
-        className='github-button m-0 h-100 p-1 text-dark btn btn-light btn-lg align-middle d-flex justify-content-between align-items-center' title={`https://github.com/jackoubridge/${props.repo}`}>
-          <span className='h-100 p-0 visit-text'>Visit Repository</span>
+        className='github-button m-0 p-1 text-dark btn btn-light btn-lg align-middle d-flex justify-content-between align-items-center my-auto' title={link}>
+          <span className='p-0 visit-text pe-1'>Visit {props.type}</span>
           <span className="icon p-0 material-symbols-outlined">captive_portal</span>
       </button>
       </>
