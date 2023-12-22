0f27ba243705fe51d89ae2ea5e367709b6c53ef3
jackoubridge <jaoubridge@gmail.com>
2023-12-22 01:42:17 +0000

    Updated sskills styling

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,7 +2,7 @@ const Skill = (props) => {
     return(
         <>
         <div className="skill bg-dark rounded d-flex justify-content-between p-2 ps-3 pe-3 m-2">
-            <span className="d-flex h-100"><img className="skill-img h-75 my-auto me-3" src={props.imageLink}></img><span className="my-auto">{props.title}</span></span>
+            <span className="d-flex h-100 skill-content"><img className="skill-img h-75 my-auto me-3" src={props.imageLink}></img><span className="my-auto skill-title">{props.title}</span></span>
         </div>
         </>
     )
