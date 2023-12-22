92100d1b73d6fe26e109756e568711dcfcba2a7d
jackoubridge <jaoubridge@gmail.com>
2023-12-22 05:58:33 +0000

    Added parallax effect. Tweaked skills style

--- src/components/Project.js
+++ src/components/Project.js
@@ -21,10 +21,9 @@ const Project = (props) => {
             <img src={props.imglink} loading="lazy" className='header-img m-0 mt-1 mb-1 p-0' alt=""></img>
           </div>
           <div className='card-body pb-0 w-100'>
-            <span className='project-info d-flex user-select-none w-100 p-0'>
+            <span className='project-info d-flex user-select-none w-100 p-0 pb-2'>
               {props.text}
             </span>
-            <hr className="mb-2 mt-3 mx-auto p-0"></hr>
             <div className='project-bottom pb-2 align-middle d-flex justify-content-between w-100'>
               <div className='badges w-auto h-100 d-flex align-middle rounded'>
                 {list}
