510095b9a2f5fb5979c6a43a45a778651871f81b
jackoubridge <jaoubridge@gmail.com>
2023-12-27 13:06:09 +0000

    Styling changes

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -2,7 +2,7 @@ const Skill = (props) => {
     return (
         <>
             <div className="skill bg-dark rounded d-flex justify-content-between p-1 ps-3 pe-3 m-1">
-                <span className="d-flex h-100 skill-content"><img className="skill-img h-75 my-auto me-3" src={props.imageLink} alt={props.title}></img><span className="my-auto skill-title">{props.title}</span></span>
+                <span className="d-flex h-100 skill-content"><img className="skill-img h-75 my-auto me-3" src={props.imageLink} alt={props.title}></img><span className="my-auto skill-title text-white">{props.title}</span></span>
             </div>
         </>
     )
