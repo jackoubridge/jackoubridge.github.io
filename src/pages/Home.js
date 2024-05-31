8db6d0cda471c41ae3b98a1d2dd6a0e9197c1313
jackoubridge <116497746+jackoubridge@users.noreply.github.com>
2024-05-31 08:52:26 +0100

    Removed about me section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,7 +28,6 @@ const Home = () => {
             <Hero scrollToProjects={scrollToProjects} />
             <div className='content m-0 p-0 w-100 h-auto rounded'>
                 <Projects ref={projectsRef} />
-                <AboutMe />
                 <Skills />
                 <Footer />
             </div>
